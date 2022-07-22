c0d28c2a242525388842710ee779f7eff83252f1
Amal Prasad <amalprasad354@gmail.com>
2022-07-22 21:16:54 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
-
+import './App.css'
 function App() {
   return (
     <div>
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div>
+        <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Banner
