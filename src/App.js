6ea0d27a2fb6290884796f13530a1e111c5e0010
Amal Prasad <amalprasad354@gmail.com>
2022-07-22 22:41:47 +0530

    Desinging Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import './App.css'
+import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
+
 function App() {
   return (
     <div>
       <NavBar/>
+      <Banner/>
+      <RowPost/>
     </div>
   );
 }
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import './Banner.css'
 function Banner() {
   return (
-    <div>
-        <h1>hello world</h1>
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Stranger Things S5</h1>
+            <div className='banner_buttons'>
+               <button className='button'>Play</button>
+               <button className='button'>My list</button>
+            </div>
+            <h1 className='discription'> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been.</h1>
+
+        </div>
+        <div className='fade_bottom'></div>
     </div>
   )
 }
